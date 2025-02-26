07cb37c7a2b7bf85322ef4e1f69814eb3c8e6a86
Cute-Fox <alex2005shevelev@gmail.com>
2025-02-27 01:37:20 +0300

    добавлена логика подсчета количества строк, удволетворяющих условию (длина больше 3)

--- yirstpp/Program.cs
+++ yirstpp/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Исходный массив строк
         string[] inputArray = { "Hello", "2", "world", ":-)" };
+
+        // Подсчитываем количество строк с длиной <= 3
+        int count = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                count++;
+            }
+        }
     }
 }
