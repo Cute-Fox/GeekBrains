17867c830c9194e2178629c3d6015b892bc26108
Cute-Fox <alex2005shevelev@gmail.com>
2025-02-27 01:43:16 +0300

    рефакторинг кода и окончательная версия программы.

--- yirstpp/Program.cs
+++ yirstpp/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Исходный массив строк (можно заменить на ввод с клавиатуры, если потребуется)
         string[] inputArray = { "Hello", "2", "world", ":-)" };
 
         // Подсчитываем количество строк с длиной <= 3
@@ -16,15 +17,14 @@ class Program
             }
         }
 
-        // Создаём новый массив нужного размера
+        // Создаём новый массив с подходящими строками
         string[] resultArray = new string[count];
         int index = 0;
         foreach (string s in inputArray)
         {
             if (s.Length <= 3)
             {
-                resultArray[index] = s;
-                index++;
+                resultArray[index++] = s;
             }
         }
 
